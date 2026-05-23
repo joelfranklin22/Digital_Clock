cf5a24dd3eb39f6038c6d5c70b22cbb814046fe5
joelfranklin22 <joeljeyam22@gmail.com>
2026-05-24 00:59:57 +0530

    feat:display current day and date

--- script.js
+++ script.js
@@ -28,7 +28,50 @@ setInterval(() => {
   document.getElementById("hours").innerHTML = hours < 10 ? "0" + hours : hours;
 }, 1000);
 
+// Day and Date
+function dayAndYear() {
+  let today = new Date();
+  const dayName = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thrusday",
+    "Friday",
+    "Saturday",
+  ];
+  const monthName = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  document.getElementById("day-year").innerHTML =
+    `${dayName[today.getDay()]},  ${monthName[today.getMonth()]} ${today.getDate()},  ${today.getFullYear()}`;
+}
 
 // Toggle On or Off
+let toggle = document.getElementById("toggle");
+let toggleis = false;
+toggle.addEventListener("click", () => {
+  toggleis = !toggleis;
+  toggle.classList.toggle("fa-toggle-on");
+  toggle.classList.toggle("fa-toggle-off");
 
-let toggle=document.getElementById("toggle-off");
+  toggle.classList.toggle("active");
+
+  if (toggleis) {
+    dayAndYear();
+  } else {
+    document.getElementById("day-year").innerHTML = "";
+  }
+});
